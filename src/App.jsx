0bc7eca053dd355377e559b98dd81541c33dde2a
jeffin poulose <jeffinpoulose2@gmail.com>
2024-06-04 20:24:38 +0530

    BREAKING CHANGE(frontend) : added new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import SideBar from "./components/SideBar";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
+  function handleToggleModal() {
+    setShowModal(!showModal);
+  }
+
+  useEffect(() => {
+    async function fetchApiData() {
+      const NASA_KEY = import.meta.env.VITE_NASA_API_KEY;
+      const url = "https://api.nasa.gov/planetary/apod?api_key=" + NASA_KEY;
+      const today=(new Date()).toDateString()
+      const localKey=`NASA-${today}`
+      if(localStorage.getItem(localKey)){
+        const apiData=JSON.parse(localStorage.getItem(localKey))
+        setData(apiData)
+        console.log('fetched from today')
+
+        return
+      }
+      localStorage.clear()
+      try {
+        const res = await fetch(url);
+        const apiData = await res.json();
+        localStorage.setItem(localKey,JSON.stringify(apiData))
+        console.log('fetcheddd');
+        setData(apiData);
+      } catch (error) {
+        console.log(error, "error in fetching");
+      }
+    }
+    fetchApiData();
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {data ? (
+        <Main data ={data} />
+      ) : (
+        <div className="loadingState">
+          {" "}
+          <i className="fa-solid fa-gear"></i>
+        </div>
+      )}{" "}
+      {showModal && <SideBar  data ={data} handleToggleModal={handleToggleModal}></SideBar>}
+      {data && <Footer data ={data} handleToggleModal={handleToggleModal} />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
